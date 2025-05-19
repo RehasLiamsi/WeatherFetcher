8592775eb84d54ce143f179ef9ab007f0802496f
RehasLiamsi <nsaher91@gmail.com>
2025-05-19 11:24:29 +0200

    Create initial controller to fetch raw data from api

--- src/main/java/com/example/weatherfetcher/controller/WeatherFetcherController.java
+++ src/main/java/com/example/weatherfetcher/controller/WeatherFetcherController.java
@@ -1,4 +1,21 @@
 package com.example.weatherfetcher.controller;
 
+import com.example.weatherfetcher.service.WeatherFetcherService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WeatherFetcherController {
+
+    private final WeatherFetcherService weatherFetcherService;
+
+    public WeatherFetcherController(WeatherFetcherService weatherFetcherService){
+        this.weatherFetcherService = weatherFetcherService;
+    }
+
+    @GetMapping("/weather")
+    public String getWeather(@RequestParam String city){
+        return weatherFetcherService.getWeather(city);
+    }
 }
